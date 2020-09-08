24ffe1fc1685460c8b34959070e96449da75cb2a
Mahdi <public.email.iran@gmail.com>
2020-09-08 09:55:30 +0430

    add js link

--- resources/views/front/index/index2.blade.php
+++ resources/views/front/index/index2.blade.php
@@ -3076,25 +3076,25 @@ END FOOTER AREA
 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA0C5etf1GVmL_ldVAichWwFFVcDfa1y_c"></script>
 <!-- inject:js -->
-<script src="js/vendor/jquery/jquery-1.12.3.js"></script>
-<script src="js/vendor/jquery/popper.min.js"></script>
-<script src="js/vendor/jquery/uikit.min.js"></script>
-<script src="js/vendor/bootstrap.min.js"></script>
-<script src="js/vendor/chart.bundle.min.js"></script>
-<script src="js/vendor/grid.min.js"></script>
-<script src="js/vendor/jquery-ui.min.js"></script>
-<script src="js/vendor/jquery.barrating.min.js"></script>
-<script src="js/vendor/jquery.countdown.min.js"></script>
-<script src="js/vendor/jquery.counterup.min.js"></script>
-<script src="js/vendor/jquery.easing1.3.js"></script>
-<script src="js/vendor/owl.carousel.min.js"></script>
-<script src="js/vendor/slick.min.js"></script>
-<script src="js/vendor/tether.min.js"></script>
-<script src="js/vendor/trumbowyg.min.js"></script>
-<script src="js/vendor/waypoints.min.js"></script>
-<script src="js/dashboard.js"></script>
-<script src="js/main.js"></script>
-<script src="js/map.js"></script>
+<script src="{{asset('darya/js/vendor/jquery/jquery-1.12.3.js')}}"></script>
+<script src="{{asset('darya/js/vendor/jquery/popper.min.js')}}"></script>
+<script src="{{asset('darya/js/vendor/jquery/uikit.min.js')}}"></script>
+<script src="{{asset('darya/js/vendor/bootstrap.min.js')}}"></script>
+<script src="{{asset('darya/js/vendor/chart.bundle.min.js')}}"></script>
+<script src="{{asset('darya/js/vendor/grid.min.js')}}"></script>
+<script src="{{asset('darya/js/vendor/jquery-ui.min.js')}}"></script>
+<script src="{{asset('darya/js/vendor/jquery.barrating.min.js')}}"></script>
+<script src="{{asset('darya/js/vendor/jquery.countdown.min.js')}}"></script>
+<script src="{{asset('darya/js/vendor/jquery.counterup.min.js')}}"></script>
+<script src="{{asset('darya/js/vendor/jquery.easing1.3.js')}}"></script>
+<script src="{{asset('darya/js/vendor/owl.carousel.min.js')}}"></script>
+<script src="{{asset('darya/js/vendor/slick.min.js')}}"></script>
+<script src="{{asset('darya/js/vendor/tether.min.js')}}"></script>
+<script src="{{asset('darya/js/vendor/trumbowyg.min.js')}}"></script>
+<script src="{{asset('darya/js/vendor/waypoints.min.js')}}"></script>
+<script src="{{asset('darya/js/dashboard.js')}}"></script>
+<script src="{{asset('darya/js/main.js')}}"></script>
+<script src="{{asset('darya/js/map.js')}}"></script>
 <!-- endinject -->
 </body>
 
